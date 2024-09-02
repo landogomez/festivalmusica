d6bea9f713e854d74a830baa657898137ab6451d
jorge <esgerry81@gmail.com>
2024-09-01 20:14:05 -0600

    Everything ready

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+import { src, dest, watch } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+
+const sass = gulpSass(dartSass)
+
+export function css( done ) {
+    src('src/img/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('dist/css'))
+
+    done()
+}
+
+export function dev() {
+    watch('src/img/scss/app.scss', css)
+}
