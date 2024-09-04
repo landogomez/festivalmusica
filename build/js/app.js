64d403f6ceffcb26438c2b48456f6d234878d25e
jorge <esgerry81@gmail.com>
2024-09-04 12:13:37 -0600

    Gallery section implemented

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,23 @@
-alert('Hola Mundo!!!')
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria()
+
+})
+
+function crearGaleria() {
+
+    const CANTIDAD_IMAGENES = 16
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for(let i = 1; i<=CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galeria'
+
+        //Event Handler
+        imagen.onclick = function() {
+            console.log('Diste Click... ',i)
+        }
+
+        galeria.appendChild(imagen)
+    }
+}
