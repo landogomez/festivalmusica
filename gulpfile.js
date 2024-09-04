785e4882906f9dedd9f4912f831edda80f05f828
jorge <esgerry81@gmail.com>
2024-09-04 11:17:00 -0600

    Incorporating JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-import { src, dest, watch } from 'gulp'
+import { src, dest, watch, series } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 
 const sass = gulpSass(dartSass)
+
+export function js(done) {
+    src('src/img/js/app.js')
+        .pipe( dest('build/js') )
+
+    done()
+}
 
 export function css( done ) {
     src('src/img/scss/app.scss', {sourcemaps: true})
@@ -15,4 +22,7 @@ export function css( done ) {
 
 export function dev() {
     watch('src/img/scss/**/*.scss', css)
+    watch('src/img/js/**/*.js', js)
 }
+
+export default series( js, css, dev)
