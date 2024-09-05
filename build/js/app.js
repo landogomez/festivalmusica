ade3f56445e0641428a2e0dfe024e147c17087dd
jorge <esgerry81@gmail.com>
2024-09-05 13:12:49 -0600

    fixed navigation and highlight the current section

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    navegacionFija()
     crearGaleria()
+    resaltarEnlace()
 
 })
+
+function navegacionFija(){
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
+
+    document.addEventListener('scroll', function() {
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+            header.classList.add('fixed')
+        } else {
+            header.classList.remove('fixed')
+        }
+    })
+}
 
 function crearGaleria() {
 
@@ -23,14 +39,19 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagen Galeria'
 
     // Generar Modal
     const modal = document.createElement('DIV')
     modal.classList.add('modal')
     modal.onclick = cerrarModal
+    modal.appendChild(imagen)
 
     //Agregar al HTML
     const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
     body.appendChild(modal)
 
 
@@ -38,6 +59,38 @@ function mostrarImagen(i) {
 
 function cerrarModal() {
     const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
 
-    modal?.remove()
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 500);
+
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section')
+        const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+        let actual = '';
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)){
+                actual = section.id
+            }
+        })
+
+        navLinks.forEach(link => {
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active')
+            }
+        })
+    })
+
+
 }
